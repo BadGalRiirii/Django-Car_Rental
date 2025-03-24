faa5892804030cfdd468cdad7e15bae464c539d7
BadGalRiirii <nicolasruilan30@gmail.com>
2025-03-24 16:31:37 +0800

    Fixed reverse() issue in api_root function

--- rental/views.py
+++ rental/views.py
@@ -2,8 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.urls import reverse
 
-from .models import Vehicle  # Import models at the top if needed
-from .serializers import VehicleSerializer  # Make sure your serializers exist
+from .models import Vehicle  
+from .serializers import VehicleSerializer  
 from rest_framework import generics
 
 class CarListCreateAPIView(generics.ListCreateAPIView):
@@ -14,9 +14,9 @@ class CarRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
 
-# Move api_root here to avoid circular import issues
+# Fix the incorrect reverse() usage
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
-        'rental_car_list': reverse('rental_car-list-create', request=request, format=format),
+        'rental_car_list': request.build_absolute_uri(reverse('rental_car-list-create'))
     })
