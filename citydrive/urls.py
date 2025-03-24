72ed86e8af27539bd0ff96f8aff5291957cc6496
BadGalRiirii <nicolasruilan30@gmail.com>
2025-03-24 14:19:03 +0800

    Integrated Django REST framework and API setup

--- citydrive/urls.py
+++ citydrive/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rental.views import CarListCreateAPIView, CarRetrieveUpdateDestroyAPIView, api_root
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', api_root, name='api-root'),  # API Root
+    path('api/v1/rental_car/', CarListCreateAPIView.as_view(), name='rental_car-list-create'),
+    path('api/v1/rental_car/<int:pk>/', CarRetrieveUpdateDestroyAPIView.as_view(), name='rental_car-retrieve-update-destroy'),
 ]
--- rental/views.py
+++ rental/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.urls import reverse
 
-# Create your views here.
+from .models import Vehicle  # Import models at the top if needed
+from .serializers import VehicleSerializer  # Make sure your serializers exist
+from rest_framework import generics
+
+class CarListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+
+class CarRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+
+# Move api_root here to avoid circular import issues
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'rental_car_list': reverse('rental_car-list-create', request=request, format=format),
+    })
